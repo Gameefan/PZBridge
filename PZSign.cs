e5ebc31a4980189096abd7fd96fda3710c8db20c
Gameefan <46563496+Gameefan@users.noreply.github.com>
2024-03-09 18:46:09 +0100

    Make PZSign arguments the same as for the other commands

--- PZSign.cs
+++ PZSign.cs
@@ -11,18 +11,17 @@ namespace PZBridge
 	{
 		public static void Execute(X509Certificate2 cert, string[] args)
 		{
-			if (args.Length < 2)
+			if (args.Length < 4)
 			{
-				Program.SendFault("sign: not enough arguments\nsyntax: sign <id> <info>\nstdin: <document> \\n EOF");
+				Program.SendFault("sign: not enough arguments\nsyntax: sign <env> <host> <id> <info>\nstdin: <document> \\n EOF");
 				return;
 			}
 
-			TpSigning5Client client = new(args[0].StartsWith("int-") ? TpSigning5Client.INTEGRATION_URI : TpSigning5Client.PRODUCTION_URI, cert);
-			string successURL = $"https://localhost/pz/success?id={args[0]}";
-			string failureURL = $"https://localhost/pz/failure?id={args[0]}";
+			TpSigning5Client client = new(args[0] == "prod" ? TpSigning5Client.PRODUCTION_URI : TpSigning5Client.INTEGRATION_URI, cert);
+			string successURL = $"{args[1]}/pz/success?id={args[2]}";
+			string failureURL = $"{args[1]}/pz/failure?id={args[2]}";
 
-
-			string additionalInfo = string.Join(' ', args[1..]);
+			string additionalInfo = string.Join(' ', args[3..]);
 
 			string document = "";
 
